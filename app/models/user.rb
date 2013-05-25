c9d9ca20ada6c520a0d4758454fcd0e018228a9c
Julian Cohen <julian@juco.co.uk>
2013-05-25 22:54:41 +0100

    Some user profile styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z\.]+\z/i
   validates :email, presence: true, length: {maximum: 50},
   			format: {with: VALID_EMAIL_REGEX},
   			uniqueness: {case_sensitive: false}
