0db7d606f1795d69f7264520730dc25651f96652
Julian Cohen <julian@juco.co.uk>
2013-05-25 21:28:55 +0100

    Gravatar image

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ RailsTutorial::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypts cost
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
