1098c8bf8fa2bf73aba01540c5aaa8bb67b1e0e1
Julian Cohen <julian@juco.co.uk>
2013-05-29 08:33:20 +0100

    Signup tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,20 +2,44 @@ require 'spec_helper'
 
 describe "User pages" do
 
-  subject { page }
+    subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+    describe "signup page" do
+        before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
+        it { should have_selector('h1',    text: 'Sign up') }
+        it { should have_selector('title', text: full_title('Sign up')) }
+    end
 
-  describe "profile page" do
-  	let(:user) { FactoryGirl.create(:user) }
-  	before { visit user_path(user) }
+    describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit user_path(user) }
 
-  	it { should have_selector('h1', text: user.name) }
-  	it { should have_selector('title', text: user.name) }
-  end
+        it { should have_selector('h1', text: user.name) }
+        it { should have_selector('title', text: user.name) }
+    end
+
+    describe "signup" do
+
+        before { visit signup_path }
+        let(:submit) { "Create my account" }
+
+        describe "with invalid data" do
+        	it "should not increment the user count with no values" do
+        		expect { click_button submit }.not_to change(User, :count)
+        	end
+        end
+
+        describe "with valid data" do
+            before do
+                fill_in "Name", with: "Example user"
+                fill_in "Email", with: "julian@juco.co.uk"
+                fill_in "Password", with: "foobar"
+                fill_in "Confirmation", with: "foobar"
+            end
+            it "should increment the user count" do
+                expect { click_button submit }.to change(User, :count).by(1)
+            end
+        end
+    end
 end
