54ecef3c09fc3f49a32fa5150e9957600f21086d
Julian Cohen <julian@juco.co.uk>
2013-06-07 13:43:14 +0100

    Gravatar helper size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-	def gravatar_for(user)
+	def gravatar_for(user, size = 100)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 end
